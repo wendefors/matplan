23c342cede62b0922ee2e3650acea3fcfe325cab
wendefors <wendefors@gmail.com>
2026-03-02 10:45:56 +0100

    Update RecipeViewer.tsx

--- components/RecipeViewer.tsx
+++ components/RecipeViewer.tsx
@@ -163,7 +163,7 @@ const RecipeViewer: React.FC = () => {
                   .map((ingredient) => (
                     <li
                       key={ingredient.id ?? `${ingredient.sortOrder}-${ingredient.name}`}
-                      className="text-xl leading-tight text-gray-800"
+                      className="text-lg leading-tight text-gray-800 [@media(orientation:landscape)]:text-base"
                     >
                       {ingredient.amount !== null
                         ? `${formatScaledAmount(ingredient.amount, servingFactor)} `
@@ -223,7 +223,7 @@ const RecipeViewer: React.FC = () => {
                           className="mt-1 h-5 w-5"
                         />
                         <span
-                          className={`text-xl leading-tight ${
+                          className={`text-lg leading-tight [@media(orientation:landscape)]:text-base ${
                             isCompleted ? "line-through text-gray-400" : "text-gray-800"
                           }`}
                         >
